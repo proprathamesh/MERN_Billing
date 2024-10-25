4fed90fcca3c416deb312db3f43f51afe02fc2ad
proprathamesh <prathameshsyadav1712@gmail.com>
2024-10-25 13:21:02 +0530

    cred

--- Billing_frontend/vite.config.ts
+++ Billing_frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Allow external access
+    host: true, // Allow external access
     port: 5173, // Change to your desired port
   },
 });
